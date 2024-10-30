4ac304d4e973da43696b964186d237de9e8731b6
Ahmet Caner Gül <cnrgl22@gmail.com>
2024-10-30 11:56:50 +0300

    Update pattern.m

--- pattern.m
+++ pattern.m
@@ -3,12 +3,14 @@ filename = "patternfile.txt";
 data = readmatrix(filename);
 pattern_flatten = squeeze(data(:,3));
 pattern_grid = 10*log10(reshape(pattern_flatten,181,360));
+pattern_grid(182:360,:) = flip(pattern_grid(2:end-1,:),1);
 
-theta = linspace(0,pi,181);
+theta = linspace(-pi,pi,360);
 phi = deg2rad(linspace(0,359,360));
 
 cut_phi = pattern_grid(91,:);
 cut_theta = pattern_grid(:,180);
+
 figure(1)
 polarplot(phi,cut_phi)
 rmin = min(cut_phi); rmax = max(cut_phi);
@@ -17,4 +19,3 @@ figure(2)
 polarplot(theta,cut_theta)
 rmin = min(cut_theta); rmax = max(cut_theta);
 rlim([rmin rmax])
-
